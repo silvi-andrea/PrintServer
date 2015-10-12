26dcb533d44b5c6e91ab6ce43181ff064cbbb7a0
Andrea <andro96.as@gmail.com>
2015-10-12 18:11:46 +0200

    print class

--- utl/Print.java
+++ utl/Print.java
@@ -1,1 +1,33 @@
-//stampa del file (il costruttore ha come parametri un InputStream)
+import java.io.InputStream;
+
+public class Print{
+
+    private InputStream in;
+
+    public Print(InputStream in) {
+        this.in = in;
+    }
+
+    public short int print(){
+        try {
+            //locate printer
+            PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
+
+            //create a print job
+            DocPrintJob job = printService.createPrintJob();
+
+            //define the format of print document
+            DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
+
+            //print the data
+            Doc doc = new SimpleDoc(in, flavor, null);
+            job.print(doc, null);
+
+            is.close();
+            return 0;
+
+        } catch (Exception e) {
+            return e.hashCode();
+        }
+    }
+}

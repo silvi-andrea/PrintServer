481aef2f11db1a36187f5b0db1beac5991b12079
Andrea Silvi <andro96.as@gmail.com>
2015-10-12 17:42:10 +0200

    scritta classe TCPServer.java

--- utl/TCPServer.java
+++ utl/TCPServer.java
@@ -3,15 +3,21 @@ import java.net.*
 public class TCPServer implements Runnable{
 	static private ServerSocket port = new ServerSocket(9999);
 	private boolean closedFlag = false;
+	private Log log=new Log();
 
 	public void run(){
-			while(!closedFlag){
+		while(!closedFlag){
 			Socket connessione = port.accept();
 			Print printer = new Print(connessione.getInputStream()); //creare classe print
-			if(printer.print()>=0){
-
-
+			short int res=printer.print();
+			if(res>=0){
+				log.logAdd("info","stampa avvenuta con successo da "+connesione.getInetAddress().getHostAddress());
+			}
+			else{
+				log.logAdd("error","errore n. "+res+" da "+connesione.getInetAddress().getHostAddress());
+			}
 		}
+		connessione.close();
 	}
 
 	public void closedConnection(){

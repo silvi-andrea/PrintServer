84dacc32163f357adfb7747399677612c7675de4
zgabe <zuddas.gabriele@gmail.com>
2015-10-12 16:21:02 +0000

    stuff...

--- utl/Print.java
+++ utl/Print.java
@@ -3,9 +3,11 @@ import java.io.InputStream;
 public class Print{
 
     private InputStream in;
+    private Log logger;
 
     public Print(InputStream in) {
         this.in = in;
+        logger = new Log();
     }
 
     public InputStream getInputStream(){
@@ -16,21 +18,26 @@ public class Print{
         try {
             //locate printer
             PrintService printService = PrintServiceLookup.lookupDefaultPrintService();
-
+            log.logStat("found system default print service.");
+            
             //create a print job
             DocPrintJob job = printService.createPrintJob();
-
+            log.logStat("creating new print job on system default print service.");
+            
             //define the format of print document
             DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
-
+            log.logStat("detected format ["+flavor+"]");
+            
             //print the data
             Doc doc = new SimpleDoc(in, flavor, null);
             job.print(doc, null);
 
             is.close();
+            log.logStat("print was successful!");
             return 0;
 
         } catch (Exception e) {
+            log.logErr(e.hasCode(), "some unexpected error occured, call a monkey wizard to solve the issue, or ask Google.");
             return e.hashCode();
         }
     }
